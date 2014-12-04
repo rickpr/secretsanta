fffdf8cb0e02995f2ea28eb2872454eebbb6dc6f
Ricardo Piro-Rael <fdisk@fdisk.co>
2014-12-04 00:48:03 -0700

    Almost working

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,3 +1,15 @@
 class Rule < ActiveRecord::Base
   belongs_to :group
+
+  validate :valid_rule?
+
+  private
+
+  def valid_rule?
+    santa=Group.find(group_id).santa
+    dimensions=santa.length
+    a=Matrix.build(dimensions, dimensions) { |row,col| row == col || (@group.Rules.where gifter: santa[row].name, recipient: santa[col].name).any? ? false : true }
+    !a.singular? # Tests for a singular matrix
+  end
+                       
 end
