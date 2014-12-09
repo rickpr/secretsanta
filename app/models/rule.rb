8e6a0b7876072828a2fff602b4b0cabe0217ae0e
Ricardo Piro-Rael <fdisk@fdisk.co>
2014-12-09 15:04:20 -0700

    Fixed some of the rules

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -9,23 +9,40 @@ class Rule < ActiveRecord::Base
     require 'matrix'
     group=Group.find group_id
     santas=group.santa
-    dimensions=santas.length
-    if gifter==recipient
-      errors.add :base, gifter + " cannot self-gift!"
-    else
-      rule_matrix=Matrix.build dimensions, dimensions do |row,col|
-        if row == col
+    gifters=santas.map &:name
+    recipients=santas.map &:name
+    flag = false
+    until gifters.empty? || flag
+      dimensions=gifters.length
+      gift_matrix=Matrix.build dimensions, dimensions do |row,col|
+        if gifters[row]==recipients[col]
           0
-        elsif (group.rules.where gifter: santas[row].name, recipient: santas[col].name).any?
+        elsif (group.rules.where gifter: group.santa.find_by_name(gifters[row]).name, recipient: group.santa.find_by_name(recipients[col]).name).any?
           0
-        elsif gifter == santas[row].name && recipient == santas[col].name
+        elsif gifter==gifters[row] && recipient==recipients[col]
           0
         else
           1
         end
       end
-      errors.add :base, "You have set too many rules" if rule_matrix.singular?
+      row_options=Array.new
+      for i in 0..gift_matrix.row_count-1 do
+        option_count = gift_matrix.row(i).inject(:+)
+        flag = true if option_count == 0
+        row_options << [option_count,i]
+      end
+      row_options=row_options.sort_by &:first
+      current_gifter=row_options.first.last
+      current_recipient=gift_matrix.row(current_gifter).find_index 1
+      if flag
+        gifters=[]
+        gift_matrix=[0]
+      else
+        gifters.slice! current_gifter
+        recipients.slice! current_recipient
+      end
     end
+    errors.add(:base, "That rule is not valid!") if gift_matrix.first==0
   end
                        
 end
